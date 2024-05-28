7d089bb01c4a22dda8203e466b480bf1fa31351a
LimYeoJin <wlsxor5828@naver.com>
2024-05-28 20:22:05 +0900

    프로젝트 최초 설정

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+create database commute default character set UTF8;
+
+-- 팀(팀이름)
+create table team(
+	id bigint auto_increment,
+    name varchar(50) not null,
+    primary key (id)
+);
+
+-- 직원(직원이름, 팀이름, 매니저인지 아닌지 여부, 회사에 들어온 일자, 생일)
+create table employee(
+	id bigint auto_increment,
+    name varchar(100) not null,
+    team_name varchar(100),
+    is_manager tinyint(1),
+    work_start_date date,
+    birthday date,
+    primary key(id)
+);
---
+++ src/test/java/com/group/commute_system/CommuteSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.commute_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuteSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
